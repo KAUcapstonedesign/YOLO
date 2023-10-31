0184c85cb3b4eff380608ecef842c42038070ce8
youngjin1590 <145269053+youngjin1590@users.noreply.github.com>
2023-10-31 17:10:52 +0900

    Add files via upload

--- face_landmark_detection.py
+++ face_landmark_detection.py
@@ -1,49 +1,39 @@
-import sys
+import cv2
+import numpy as np #배열 계산 용이
+from PIL import Image #python imaging library
 import os
-import dlib
-import glob
 
-if len(sys.argv) != 3:
-    print(
-        "Give the path to the trained shape predictor model as the first "
-        "argument and then the directory containing the facial images.\n"
-        "For example, if you are in the python_examples folder then "
-        "execute this program by running:\n"
-        "    ./face_landmark_detection.py shape_predictor_68_face_landmarks.dat ../examples/faces\n"
-        "You can download a trained facial shape predictor from:\n"
-        "    http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2")
-    exit()
+path = 'dataset' #경로 (dataset 폴더)
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+detector = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
 
-predictor_path = sys.argv[1]    # 랜드마크 파일 경로
-faces_folder_path = sys.argv[2] # 이미지 경로
+def getImagesAndLabels(path):
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+    #listdir : 해당 디렉토리 내 파일 리스트
+    #path + file Name : 경로 list 만들기
 
-detector = dlib.get_frontal_face_detector()  # 얼굴 인식용 클래스 생성 (기본 제공되는 얼굴 인식 모델 사용)
-predictor = dlib.shape_predictor(predictor_path)    # 인식된 얼굴에서 랜드마크 찾기위한 클래스 생성
-win = dlib.image_window()   # 이미지를 화면에 표시하기 위한 윈도 생성
+    faceSamples = []
+    ids = []
+    for imagePath in imagePaths: #각 파일마다
+        #흑백 변환
+        PIL_img = Image.open(imagePath).convert('L') #L : 8 bit pixel, bw
+        img_numpy = np.array(PIL_img, 'uint8')
 
-for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):   # jpg 파일만 찾음
-    print("Processing file: {}".format(f))  # 파일 경로 출력
-    img = dlib.load_rgb_image(f)    # 파일을 img 변수에 저장
+        #user id
+        id = int(os.path.split(imagePath)[-1].split(".")[1])#마지막 index : -1
 
-    win.clear_overlay() # 화면 지우기
-    win.set_image(img)  # img를 화면에 출력
+        #얼굴 샘플
+        faces = detector.detectMultiScale(img_numpy)
+        for(x,y,w,h) in faces:
+            faceSamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
 
-    # Ask the detector to find the bounding boxes of each face. The 1 in the
-    # second argument indicates that we should upsample the image 1 time. This
-    # will make everything bigger and allow us to detect more faces.
-    dets = detector(img, 1) # 1은 업샘플링 횟수
-    print("Number of faces detected: {}".format(len(dets))) # 인식된 얼굴 개수 출력
-    for k, d in enumerate(dets):        # 인식된 얼굴 개수만큼 반복하여 얼굴 윤곽 표시  
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format( 
-            k, d.left(), d.top(), d.right(), d.bottom()))   # 인식된 좌표 출력
-        # Get the landmarks/parts for the face in box d.
-        shape = predictor(img, d)    # 인식된 좌표로부터 랜드마크 추출
-        print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
-                                                  shape.part(1)))   # 랜드마크 중 처음 2개 출력
-        # Draw the face landmarks on the screen.
-        win.add_overlay(shape)  # 랜드마크 화면 출력
+    return faceSamples, ids
 
-    win.add_overlay(dets)   # 얼굴 윤곽 화면 출력
-    dlib.hit_enter_to_continue()    # 다음 이미지로 넘어가기 위해 엔터키 입력
+print('\n [INFO] Training faces. It will take a few seconds. Wait ...')
+faces, ids = getImagesAndLabels(path)
 
-#python face_landmark_detection.py shape_predictor_68_face_landmarks.dat ./
+recognizer.train(faces,np.array(ids)) #학습
+
+recognizer.write('trainer/trainer.yml')
+print('\n [INFO] {0} faces trained. Exiting Program'.format(len(np.unique(ids))))
