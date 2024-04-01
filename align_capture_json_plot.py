12239244b8e0b1060148bf4d7438e612b7c89a65
Hyunsoo Kim <71876604+Kimhyunsoo2497@users.noreply.github.com>
2024-04-01 14:58:02 +0900

    Update align_capture_json_plot.py
    if문 제거해서 얼굴 검출될 때만 캡쳐한다는 조건 없앰
    -> 기울어져서 100장이 모두 안 찍히는 문제 해결
    -> 그러나 json파일이 누락되는 문제 발생

--- align_capture_json_plot.py
+++ align_capture_json_plot.py
@@ -17,6 +17,8 @@ face_id = input('\n enter user id end press <enter> ==> ')
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
 count = 0
 
+json_files = [] # JSON 파일 경로를 저장할 리스트
+
 # 얼굴 정렬 함수
 def align_faces(frame):
     dets = detector(frame, 1)
@@ -27,8 +29,6 @@ def align_faces(frame):
         faces.append(face_chip)
     return faces
 
-json_files = [] # JSON 파일 경로를 저장할 리스트
-
 # 얼굴 캡처 및 랜드마크 추출
 while True:
     ret, frame = capture.read()
@@ -38,26 +38,26 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray, 1.05, 3, minSize=(20,20))
     
-    # 얼굴 정렬 및 저장
-    if len(faces) > 0:
+    # 얼굴 정렬
+    aligned_faces = align_faces(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    
+    # 얼굴 랜드마크 추출 및 JSON 파일 저장
+    for aligned_face in aligned_faces:
+        aligned_gray = cv2.cvtColor(aligned_face, cv2.COLOR_RGB2GRAY)
         count += 1
-        aligned_faces = align_faces(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        img_path = f"dataset/face_{face_id}_{count}.jpg"
+        cv2.imwrite(img_path, aligned_gray)
         
-        for aligned_face in aligned_faces:
-            aligned_gray = cv2.cvtColor(aligned_face, cv2.COLOR_RGB2GRAY)
-            img_path = f"dataset/face_{face_id}_{count}.jpg"
-            cv2.imwrite(img_path, aligned_gray)
-            
-            # 랜드마크 추출 및 JSON 파일 저장
-            rects = detector(aligned_gray, 1)
-            for rect in rects:
-                landmarks = predictor(aligned_gray, rect)
-                landmark_list = [[p.x, p.y] for p in landmarks.parts()]
+        # 랜드마크 추출 및 JSON 파일 저장
+        rects = detector(aligned_gray, 1)
+        for rect in rects:
+            landmarks = predictor(aligned_gray, rect)
+            landmark_list = [[p.x, p.y] for p in landmarks.parts()]
 
-                json_path = f"dataset/face_{face_id}_{count}.json"
-                json_files.append(json_path)  # JSON 파일 경로 저장
-                with open(json_path, "w") as json_file:
-                    json.dump(landmark_list, json_file)
+            json_path = f"dataset/face_{face_id}_{count}.json"
+            json_files.append(json_path)  # JSON 파일 경로 저장
+            with open(json_path, "w") as json_file:
+                json.dump(landmark_list, json_file)
 
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) == 27 or count >= 100:  # ESC 키를 누르거나 100회 캡처 시 종료
